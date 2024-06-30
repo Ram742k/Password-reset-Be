8b5a41bcee6c22c33a2ddef9842443b1997d0486
Ram742k <ramkumar742k@gmail.com>
2024-06-30 19:44:57 +0530

    cor access add

--- app.js
+++ app.js
@@ -11,11 +11,27 @@ const corsOptions = {
   origin: 'https://eclectic-strudel-2ca918.netlify.app/', // Change this to the correct origin without trailing slash
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // if you need to send cookies
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
+  allowedHeaders: 'Origin, Content-Type, X-Auth-Token',
 };
 
 app.use(cors(corsOptions));
-
+app.use((req, res, next) => {
+    res.set({
+      "Access-Control-Allow-Origin": "https://password-reset-fe-flow.netlify.app",
+      "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+    });
+    
+    if (req.method === 'OPTIONS') {
+      res.set({
+        "Access-Control-Allow-Credentials": "true"
+      });
+      return res.status(204).send(); // respond to preflight requests
+    }
+  
+    next();
+  });
 // Log requests
 app.use(morgan('dev'));
 
